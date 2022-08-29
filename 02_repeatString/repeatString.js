48f3b1b192b5fe45efc74a2c6d19217aed6dffcc
mikeson <mikeson.jnr@gmail.com>
2022-08-29 20:53:07 +0100

    Completed test 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
 const repeatString = function(string, num) {
+  newString = '';
 
+  if (num < 0){
+    return "ERROR";
+  } else if (string == " "){
+    return  "";
+  }
+
+  for(let i = 0; i < num; i++){
+    newString += string;
+  }
+
+  return newString;
 };
 
 // Do not edit below this line
