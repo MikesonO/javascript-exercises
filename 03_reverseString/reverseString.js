fadb2a429eda22bd3c2669b61b2e10a51f3a1cf4
mikeson <mikeson.jnr@gmail.com>
2022-08-29 21:05:27 +0100

    Completed test 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(string) {
 
+  let splitString = string.split(""); //Splits the string into array
+  let reverseString = splitString.reverse(); //Reverses the order of the array
+  let joinString = reverseString.join(""); //Joins the array to make reversed string
+
+  return joinString
 };
 
 // Do not edit below this line
