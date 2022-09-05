983c31f5d5636a75973e07427d56f2473fa6b028
mikeson <mikeson.jnr@gmail.com>
2022-09-05 22:38:41 +0100

    Complete test 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let newString = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+    newString = newString.replace(/\s/g, '');
+  let reverseStr = newString.split('').reverse().join(''); 
+    if(reverseStr === newString){
+        return true;
+    } else{
+        return false;
+    }
+    
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
