24a521d9a6d4faae4c620f91d10c017de77451e8
mikeson <mikeson.jnr@gmail.com>
2022-09-05 22:04:37 +0100

    Complete test 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,32 @@
-const fibonacci = function() {
+const { add } = require("../08_calculator/calculator");
 
-};
+const fibonacci = function(number) {
+
+let fibo = 1;
+let add = 1;
+let fiboAdd = 0;
+
+if(number < 0){
+  return "OOPS";
+} else if (typeof number === "string" ){
+  parseInt(number);
+}
+
+for(let i = 1; i < number; i++){
+ 
+  fiboAdd = fibo + add; // = 2
+  if (i >= 1){
+  fibo = add;
+}
+  add = fiboAdd;
+}
+return fibo;
+}
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
